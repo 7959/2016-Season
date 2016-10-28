5fb2d80b9f5fe41814cdece020a9f3de332c77a8
JoeDaTomato <josephholdom@gmail.com>
2016-10-28 10:20:13 -0400

    Made slightly faster.

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/teleop.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/teleop.java
@@ -1,11 +1,9 @@
 package org.firstinspires.ftc.robotcontroller.internal;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
@@ -31,8 +29,6 @@ public class teleop extends OpMode {
     private Servo launcherUD;
 
     private TouchSensor loadTester;
-
-    private boolean gamepad1turn = false;
 
     @Override
     public void init() {
@@ -85,7 +81,6 @@ public class teleop extends OpMode {
             backL.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x);
             backR.setPower(gamepad1.left_stick_y - gamepad1.left_stick_x);
             telemetry.addData("ROBOT SPEED", 100 * (Math.abs(gamepad1.left_stick_y) + Math.abs(gamepad1.left_stick_x)) + "%");
-            gamepad1turn = gamepad1.left_stick_x != 0 || gamepad1.left_stick_y != 0;
         } else if (gamepad1.right_bumper) {
             frontL.setPower(1);
             frontR.setPower(-1);
@@ -108,7 +103,7 @@ public class teleop extends OpMode {
             poker.setPower(0);
         }
 
-        
+
         if (!loadTester.isPressed()) {
             if (gamepad2.x) {
                 reloader.setPower(0.1);
@@ -141,7 +136,7 @@ public class teleop extends OpMode {
             launcherUD.setPosition(launcherUD.getPosition() - 0.01);
         }
 
-        if (!gamepad1turn) {
+        if (gamepad1.left_stick_x == 0 && gamepad1.left_stick_y == 0) {
             if (gamepad2.dpad_left) {
                 frontL.setPower(-1);
                 frontR.setPower(1);
