663aa9d84a77870b9ba9b9eaf660280d186bc878
Robi <robihuq3@gmail.com>
2016-12-08 19:37:35 -0500

    When I wake up, well, I know I'm gonna be I'm gonna be the man who wakes up next to you When I go out, yeah, I know I'm gonna be I'm gonna be the man who goes along with you If I get drunk, well, I know I'm gonna be I'm gonna be the man who gets drunk next to you And if I haver, hey, I know I'm gonna be I'm gonna be the man who's havering to you
    But I would walk five hundred miles
    And I would walk five hundred more
    Just to be the man who walked a thousand miles
    To fall down at your door
    
    When I'm working, yes, I know I'm gonna be
    I'm gonna be the man who's working hard for you
    And when the money comes in for the work I do
    I'll pass almost every penny on to you
    When I come home (When I come home), oh, I know I'm gonna be
    I'm gonna be the man who comes back home to you
    And if I grow old, well, I know I'm gonna be
    I'm gonna be the man who's growing old with you
    
    But I would walk five hundred miles
    And I would walk five hundred more
    Just to be the man who walked a thousand miles
    To fall down at your door
    
    Da lat da (Da lat da), da lat da (Da lat da)
    Da-da-da dun-diddle un-diddle un-diddle uh da-da
    Da lat da (Da lat da), da lat da (Da lat da)
    Da-da-da dun-diddle un-diddle un-diddle uh da-da
    
    When I'm lonely, well, I know I'm gonna be
    I'm gonna be the man who's lonely without you
    And when I'm dreaming, well, I know I'm gonna dream
    I'm gonna dream about the time when I'm with you
    When I go out (When I go out), well, I know I'm gonna be
    I'm gonna be the man who goes along with you
    And when I come home (When I come home), yes, I know I'm gonna be
    I'm gonna be the man who comes back home with you
    I'm gonna be the man who's coming home with you
    
    But I would walk five hundred miles
    And I would walk five hundred more
    Just to be the man who walked a thousand miles
    To fall down at your door
    
    Da lat da (Da lat da), da lat da (Da lat da)
    Da-da-da dun-diddle un-diddle un-diddle uh da-da
    Da lat da (Da lat da), da lat da (Da lat da)
    Da-da-da dun-diddle un-diddle un-diddle uh da-da
    Da lat da (Da lat da), da lat da (Da lat da)
    Da-da-da dun-diddle un-diddle un-diddle uh da-da
    Da lat da (Da lat da), da lat da (Da lat da)
    Da-da-da dun-diddle un-diddle un-diddle uh da-da
    
    And I would walk five hundred miles
    And I would walk five hundred more
    Just to be the man who walked a thousand miles
    To fall down at your door

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/AutoRedAll.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/AutoRedAll.java
@@ -14,17 +14,13 @@ package org.firstinspires.ftc.robotcontroller.internal;
 public class AutoRedAll extends LinearOpMode {
     private DcMotor frontL; // 1
     private DcMotor frontR; // 2
-
     private DcMotor backL; // 3
     private DcMotor backR; // 4
-
     private DcMotor middleL; // 5
     private DcMotor middleR; // 6
-
     private ColorSensor sensor1;
     private ColorSensor sensor2;
     private OpticalDistanceSensor dsensor;
-
     boolean Thing = false;
     int Thingy = 0;
 
@@ -32,43 +28,43 @@ public class AutoRedAll extends LinearOpMode {
     public void runOpMode() {
 
         dsensor = hardwareMap.opticalDistanceSensor.get("Distance Sensor");
-
+        
         frontL = hardwareMap.dcMotor.get("Front Left"); // 1
         frontR = hardwareMap.dcMotor.get("Front Right"); // 2
-
+        
         middleL = hardwareMap.dcMotor.get("Middle Left"); // 3
         middleR = hardwareMap.dcMotor.get("Middle Right"); // 4
-
+        
         backL = hardwareMap.dcMotor.get("Back Left"); // 5
         backR = hardwareMap.dcMotor.get("Back Right"); // 6
-
+        
         sensor1 = hardwareMap.colorSensor.get("Up Sensor");
         sensor2 = hardwareMap.colorSensor.get("Down Sensor");
-
+        
         frontL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // 1
         frontR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // 2
-
+        
         middleL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // 3
         middleR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // 4
-
+        
         backL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // 5
         backR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //
-
+        
         frontL.setDirection(DcMotorSimple.Direction.REVERSE); // 1
         frontR.setDirection(DcMotorSimple.Direction.REVERSE); // 2
-
+        
         middleL.setDirection(DcMotorSimple.Direction.REVERSE); // 4
         middleR.setDirection(DcMotorSimple.Direction.FORWARD); // 3
-
+        
         backL.setDirection(DcMotorSimple.Direction.REVERSE); // 5
         backR.setDirection(DcMotorSimple.Direction.FORWARD); // 6
-
+        
         frontL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // 1
         frontR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // 2
-
+        
         middleL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // 3
         middleR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // 4
-
+        
         backL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // 5
         backR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // 6
     }
@@ -253,7 +249,7 @@ public class AutoRedAll extends LinearOpMode {
             telemetry.addData("Error", "Would not sleep");
         }
     }
-    public void Traffic_aint_gunna_get_in_my_way_again_I_will_buy_an_off_road_truck_and_go_on_long_journeys_into_the_countrside(){
+    public void When_I_wake_up_well_I_know_Im_gonna_be_Im_gonna_be_the_man_who_wakes_up_next_to_you_When_I_go_out_yeah_I_know_Im_gonna_be_Im_gonna_be_the_man_who_goes_along_with_you_If_I_get_drunk_well_I_know_Im_gonna_be_Im_gonna_be_the_man_who_gets_drunk_next_to_you_And_if_I_haver_hey_I_know_Im_gonna_be_Im_gonna_be_the_man_whos_havering_to_you_But_I_would_walk_five_hundred_miles_Dada_da_dun_diddle_un_diddle_un_diddle_uh_da_da_When_Im_lonely_well_I_know_Im_gonna_be_Im_gonna_be_the_man_whos_lonely_without_you_And_when_Im_dreaming_well_I_know_Im_gonna_dream_Im_gonna_dream_about_the_time_when_Im_with_you_When_I_go_out_When_I_go_out_well_I_know_Im_gonna_be_Im_gonna_be_the_man_who_goes_along_with_you_And_when_I_come_home_When_I_come_home_yes_I_know_Im_gonna_be_Im_gonna_be_the_man_who_comes_back_home_with_you_Im_gonna_be_the_man_whos_coming_home_with_you_But_I_would_walk_five_hundred_miles_And_I_would_walk_five_hundred_more_Just_to_be_the_man_who_walked_a_thousand_miles_To_fall_down_at_your_door_Da_lat_da_Da_lat_da_da_lat_da_Da_lat_da_Da_da_da_dun_diddle_un_diddle_un_diddle_uh_da_da_Da_lat_da_Da_lat_da_da_lat_da_Da_lat_da_Da_da_da_dun_diddle_un_diddle_un_diddle_uh_da_da_Da_lat_da_Da_lat_da_da_lat_da_Da_lat_da_Da_da_da_dun_diddle_un_diddle_un_diddle_uh_da_da_Da_lat_da_Da_lat_da_da_lat_da_Da_lat_da_Da_da_da_dun_diddle_un_diddle_un_diddle_uh_da_da_And_I_would_walk_five_hundred_miles_And_I_would_walk_five_hundred_more_Just_to_be_the_man_who_walked_a_thousand_miles_To_fall_down_at_your_door(){
         frontL.setPower(-1);
         frontR.setPower(1);
         middleR.setPower(1);
@@ -310,7 +306,7 @@ public class AutoRedAll extends LinearOpMode {
         Contraption();
         PushymcThingy();
         GETOFFMYLAWNYEDARNKIDS();
-        Traffic_aint_gunna_get_in_my_way_again_I_will_buy_an_off_road_truck_and_go_on_long_journeys_into_the_countrside();
+        When_I_wake_up_well_I_know_Im_gonna_be_Im_gonna_be_the_man_who_wakes_up_next_to_you_When_I_go_out_yeah_I_know_Im_gonna_be_Im_gonna_be_the_man_who_goes_along_with_you_If_I_get_drunk_well_I_know_Im_gonna_be_Im_gonna_be_the_man_who_gets_drunk_next_to_you_And_if_I_haver_hey_I_know_Im_gonna_be_Im_gonna_be_the_man_whos_havering_to_you_But_I_would_walk_five_hundred_miles_Dada_da_dun_diddle_un_diddle_un_diddle_uh_da_da_When_Im_lonely_well_I_know_Im_gonna_be_Im_gonna_be_the_man_whos_lonely_without_you_And_when_Im_dreaming_well_I_know_Im_gonna_dream_Im_gonna_dream_about_the_time_when_Im_with_you_When_I_go_out_When_I_go_out_well_I_know_Im_gonna_be_Im_gonna_be_the_man_who_goes_along_with_you_And_when_I_come_home_When_I_come_home_yes_I_know_Im_gonna_be_Im_gonna_be_the_man_who_comes_back_home_with_you_Im_gonna_be_the_man_whos_coming_home_with_you_But_I_would_walk_five_hundred_miles_And_I_would_walk_five_hundred_more_Just_to_be_the_man_who_walked_a_thousand_miles_To_fall_down_at_your_door_Da_lat_da_Da_lat_da_da_lat_da_Da_lat_da_Da_da_da_dun_diddle_un_diddle_un_diddle_uh_da_da_Da_lat_da_Da_lat_da_da_lat_da_Da_lat_da_Da_da_da_dun_diddle_un_diddle_un_diddle_uh_da_da_Da_lat_da_Da_lat_da_da_lat_da_Da_lat_da_Da_da_da_dun_diddle_un_diddle_un_diddle_uh_da_da_Da_lat_da_Da_lat_da_da_lat_da_Da_lat_da_Da_da_da_dun_diddle_un_diddle_un_diddle_uh_da_da_And_I_would_walk_five_hundred_miles_And_I_would_walk_five_hundred_more_Just_to_be_the_man_who_walked_a_thousand_miles_To_fall_down_at_your_door();
         requestOpModeStop();
     }
 }
