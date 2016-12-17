a7bd4f62c8ad540194dfafef355ccee9ac544d37
Joseph Holdom <josephholdom@protonmail.ch>
2016-12-17 13:43:57 -0500

    Added base OpModes for our robot in both normal and linear opmodes

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Krusher99.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+
+/**
+ * Created by Joseph on 12/17/2016.
+ */
+
+public abstract class Krusher99 extends OpMode {
+    public DcMotor frontL; // 1
+    public DcMotor frontR; // 2
+    public DcMotor backL; // 3
+    public DcMotor backR; // 4
+    public DcMotor middleL; // 5
+    public DcMotor middleR; // 6
+    public ColorSensor sensor1;
+    public ColorSensor sensor2;
+    public GyroSensor Gsensor;
+    public OpticalDistanceSensor ODsensor;
+    @Override
+    public void init() {
+        frontL = hardwareMap.dcMotor.get("Front Left"); // 1
+        frontR = hardwareMap.dcMotor.get("Front Right"); // 2
+        middleL = hardwareMap.dcMotor.get("Middle Left"); // 3
+        middleR = hardwareMap.dcMotor.get("Middle Right"); // 4
+        backL = hardwareMap.dcMotor.get("Back Left"); // 5
+        backR = hardwareMap.dcMotor.get("Back Right"); // 6
+        sensor1 = hardwareMap.colorSensor.get("Up Sensor");
+        sensor2 = hardwareMap.colorSensor.get("Down Sensor");
+        Gsensor = hardwareMap.gyroSensor.get("Gyro Sensor");
+        ODsensor = hardwareMap.opticalDistanceSensor.get("OD Sensor");
+        frontL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // 1
+        frontR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // 2
+        middleL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // 3
+        middleR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // 4
+        backL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // 5
+        backR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // 6
+        frontL.setDirection(DcMotorSimple.Direction.FORWARD); // 1
+        frontR.setDirection(DcMotorSimple.Direction.FORWARD); // 2
+        middleL.setDirection(DcMotorSimple.Direction.FORWARD); // 4
+        middleR.setDirection(DcMotorSimple.Direction.REVERSE); // 3
+        backL.setDirection(DcMotorSimple.Direction.FORWARD); // 5
+        backR.setDirection(DcMotorSimple.Direction.REVERSE); // 6
+        frontL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // 1
+        frontR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // 2
+        middleL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // 3
+        middleR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // 4
+        backL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // 5
+        backR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // 6
+    }
+}
