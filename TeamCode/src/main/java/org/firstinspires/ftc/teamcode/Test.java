74e732f142bd0f0eec46152684acf147cc16414a
Joseph Holdom <josephholdom@protonmail.ch>
2017-01-16 13:23:53 -0500

    ?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchDevice;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
 import com.qualcomm.robotcore.hardware.I2cController;
@@ -23,9 +24,12 @@ public class Test extends OpMode {
     protected DcMotor middleL; // 5
     protected DcMotor middleR; // 6
     protected DcMotor launcher;
+    protected DcMotor launcher2;
     protected ColorSensor sensor1;
     protected ColorSensor sensor2;
     protected GyroSensor Gsensor;
+    protected DeviceInterfaceModule dim;
+    private int P = 0;
     protected OpticalDistanceSensor ODsensor;
     public void init() {
         /*frontL = hardwareMap.dcMotor.get("Front Left"); // 1
@@ -34,8 +38,12 @@ public class Test extends OpMode {
         middleR = hardwareMap.dcMotor.get("Middle Right"); // 4
         backL = hardwareMap.dcMotor.get("Back Left"); // 5
         backR = hardwareMap.dcMotor.get("Back Right"); // 6
-        launcher = hardwareMap.dcMotor.get("Launcher");
-        */sensor1 = hardwareMap.colorSensor.get("Up Sensor");
+        */launcher = hardwareMap.dcMotor.get("Launcher");
+
+        dim = hardwareMap.deviceInterfaceModule.get("dim");
+
+        launcher2 = hardwareMap.dcMotor.get("Launcher2");
+        sensor1 = hardwareMap.colorSensor.get("Up Sensor");
         sensor1.setI2cAddress(I2cAddr.create8bit(0x40));
         sensor2 = hardwareMap.colorSensor.get("Down Sensor");
         sensor2.setI2cAddress(I2cAddr.create8bit(0x46));
@@ -47,8 +55,10 @@ public class Test extends OpMode {
         middleR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // 4
         backL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // 5
         backR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // 6
+        */
         launcher.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        frontL.setDirection(DcMotorSimple.Direction.FORWARD); // 1
+        launcher2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        /*.setDirection(DcMotorSimple.Direction.FORWARD); // 1
         frontR.setDirection(DcMotorSimple.Direction.FORWARD); // 2
         middleL.setDirection(DcMotorSimple.Direction.FORWARD); // 4
         middleR.setDirection(DcMotorSimple.Direction.REVERSE); // 3
@@ -60,8 +70,9 @@ public class Test extends OpMode {
         middleR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // 4
         backL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // 5
         backR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // 6
+        */launcher2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         launcher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        */telemetry.addData("sg2", sensor2.green());
+        telemetry.addData("sg2", sensor2.green());
         telemetry.addData("sr2", sensor2.red());
         telemetry.addData("sb2", sensor2.blue());
         telemetry.addData("timer", getRuntime());
@@ -76,6 +87,9 @@ public class Test extends OpMode {
         telemetry.addData("conninfo1", sensor1.getConnectionInfo());
         telemetry.addData("conninfo2", sensor2.getConnectionInfo());
         telemetry.addData("Gconninfo", Gsensor.getConnectionInfo());
+        dim.setLED(0x0, false);
+        dim.setLED(0x1, true);
+        dim.setLED(0x2, true);
     }
     public void loop(){
         telemetry.addData("sg2", sensor2.green());
@@ -93,6 +107,12 @@ public class Test extends OpMode {
         telemetry.addData("conninfo1", sensor1.getConnectionInfo());
         telemetry.addData("conninfo2", sensor2.getConnectionInfo());
         telemetry.addData("Gconninfo", Gsensor.getConnectionInfo());
+        telemetry.addData("launcher Power", launcher.getPower());
+        if(Gsensor.rawY()+Gsensor.rawX()+Gsensor.rawZ()>10000){
+            launcher2.setPower(P +.1);
+            launcher.setPower(P+.1);
+        }
+
 
 
     }
