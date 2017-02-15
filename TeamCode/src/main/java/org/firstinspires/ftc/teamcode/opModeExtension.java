68ecff1b1e4815e75860c56530cb31d471bf12a0
Joseph Holdom <josephholdom@protonmail.ch>
2017-02-15 16:03:18 -0500

    STUFF

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModeExtension.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by Joseph on 2/15/2017.
+ */
+
+abstract public class opModeExtension extends OpMode {
+    protected DcMotor frontLeft;
+    protected DcMotor frontRight;
+    protected DcMotor backLeft;
+    protected DcMotor backRight;
+    public void init() {
+        
+    }
+}
